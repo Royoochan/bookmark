a58dea79b81fa6724e67929fefc18f2671ee3caa
Royoochan <m01076242700@gmail.com>
2023-08-24 20:57:47 +0900

    bookmark service

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
